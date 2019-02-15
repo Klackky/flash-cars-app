6ffc5d52fedd8c1820d874dc7142c46356a9ee59
Natalia <natalia.volchatova@gmail.com>
2019-02-15 15:31:36 +0100

    filter

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const questionArea = document.getElementById("flash-card")
 let randomNumberCard;
 
 nextButton.onclick = function nextCard () {
-    randomNumberCard = Math.floor(Math.random()*data.length)
-    questionArea.innerHTML = data[randomNumberCard].question
-
+    const filteredCards = data.filter(card => select.options[select.selectedIndex].text == card.category);
+    randomNumberCard = Math.floor(Math.random()*filteredCards.length)
+    questionArea.innerHTML = filteredCards[randomNumberCard].question
 }
 
 // flip button
@@ -22,20 +22,35 @@ flipButton.onclick = function flipCard() {
 
 
 const form = document.querySelector(`form`);
+const categoryField = document.querySelector(`#category`);
 const questionField = document.querySelector(`#question`);
 const answerField = document.querySelector(`#answer`);
 const deleteButton = document.querySelector(`#delete`);
+const select = document.querySelector(`#select`);
+
+
 
 const createCard = () => {
     let card = {};
     card.question = questionField.value;
     card.answer = answerField.value;
+    card.category = categoryField.value;
     data.push(card);
     return data;
 }
 
+const addSelectors = () => {
+    const isSelectorAlreadyExists = data.some(card => categoryField.value === card.category);
+    if(!isSelectorAlreadyExists) {
+      const option = document.createElement("option");
+      option.text = categoryField.value;
+      select.add(option);
+   }  
+}
+
 form.addEventListener(`submit`, (event) => {
     event.preventDefault();
+    addSelectors();
     createCard();
     console.log(data);
     form.reset();
@@ -46,4 +61,8 @@ deleteButton.addEventListener(`click`, () => {
     data.splice(randomNumberCard, 1);
     questionArea.innerHTML = "";
 });
+
+
+
+
 
