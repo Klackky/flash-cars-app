7c092691e4c831c191944274bc9ea42c64d52013
Natalia <natalia.volchatova@gmail.com>
2019-02-15 16:10:32 +0100

    final

--- index.js
+++ index.js
@@ -34,8 +34,9 @@ const questionArea = document.getElementById("flash-card")
 let randomNumberCard;
 
 nextButton.onclick = function nextCard () {
-    randomNumberCard = Math.floor(Math.random()*data.length)
-    questionArea.innerHTML = data[randomNumberCard].question
+    const filteredCards = data.filter(card => select.options[select.selectedIndex].text == card.category);
+    randomNumberCard = Math.floor(Math.random()*filteredCards.length)
+    questionArea.innerHTML = filteredCards[randomNumberCard].question
     document.getElementById('good-answer').innerHTML = "" // reset text to user when answer good
     typeForm.reset() // reset answer form
 }
@@ -49,21 +50,37 @@ flipButton.onclick = function flipCard() {
 
 
 
+
+const categoryField = document.querySelector(`#category`);
 const form = document.querySelector(`.add-card`);
 const questionField = document.querySelector(`#question`);
 const answerField = document.querySelector(`#answer`);
 const deleteButton = document.querySelector(`#delete`);
+const select = document.querySelector(`#select`);
+
+
 
 const createCard = () => {
     let card = {};
     card.question = questionField.value;
     card.answer = answerField.value;
+    card.category = categoryField.value;
     data.push(card);
     return data;
 }
 
+const addSelectors = () => {
+    const isSelectorAlreadyExists = data.some(card => categoryField.value === card.category);
+    if(!isSelectorAlreadyExists) {
+      const option = document.createElement("option");
+      option.text = categoryField.value;
+      select.add(option);
+   }  
+}
+
 form.addEventListener(`submit`, (event) => {
     event.preventDefault();
+    addSelectors();
     createCard();
     console.log(data);
     form.reset();
@@ -74,6 +91,7 @@ deleteButton.addEventListener(`click`, () => {
     data.splice(randomNumberCard, 1);
     questionArea.innerHTML = "";
 });
+
 
 // showing dancing banana when anwer is good
 function show() {
@@ -98,4 +116,3 @@ typeForm.addEventListener('submit', function(event) {
         document.getElementById('good-answer').innerHTML = "This is not the rigth answer. Try again or flip card"
     }
 })
-
