9d7967f63b87ce17876943ee64d5b99f581b4b40
vdegraaf <vdegraaf.vincent@gmail.com>
2019-02-15 13:28:09 +0100

    next-button almost finished

--- index.js
+++ index.js
@@ -2,7 +2,32 @@ const data = [
     {
         question: `do you have an answer?`,
         answer: `no`
+    },
+    {
+        question: `How are you?`,
+        answer: `good`
+    },
+    {
+        question: `Is it Friday?`,
+        answer: `yes`
     }
+
 ]
+
+
+const nextButton = document.getElementsByClassName("next")[0]
+
+nextButton.onclick = function nextCard () {
+    const question = document.createElement('h2')
+    // console.log(question)
+    const randomNumber = Math.floor(Math.random()*data.length)
+    // console.log(randomNumber)
+    question.innerHTML = data[randomNumber].question
+    // console.log(question.innerHTML)
+    const flashCardSection = document.getElementById("flash-card")
+    flashCardSection.appendChild(question)
+    // console.log(flashCardSection)
+
+}
 
 
